2e1242de06f02e8d6eedbb25cd8383af7fd33453
Tulod CHristian <tulodchristianjoshua1@gmail.com>
2026-02-02 09:52:57 +0800

    Initial commit

---
+++ button.js
@@ -0,0 +1,11 @@
+docuument.getElementById("btn").addEventListener("click", save);
+
+/*************  ✨ Windsurf Command 🌟  *************/
+/**
+ * Save function
+ * 
+ * When the button is clicked, it will alert the user that the button was clicked.
+ */
+function save() {
+    alert("YOU CLICKED THE BUTTON");
+}
